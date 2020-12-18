63d4267e4f3b2f1f21e591c7c583ac4d891cdddb
Sachin <sachinlaish@gmail.com>
2020-12-18 11:40:05 +0530

    done

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,11 @@
 
 
 // export class using module.exports
+
+
+
+let Shape = require("./shape");
+
+class Circle extends Shape{
+    calculateArea = () => {console.log("area of circle")};
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+
+class Shape {
+    
+    color = "black";
+
+    drawShape = () => {};
+
+    calculateArea = () => {};
+
+}
+
+module.exports(Shape);
